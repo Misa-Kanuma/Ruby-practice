c40d8769f096d0adead66f12320c4997bb8147fe
Misa-Kanuma <misa.kanuma@gmail.com>
2020-06-06 19:12:27 +0900

    [Finish] 2-8 chapter8_ercise,chapter8_exercise2　解答修正

--- Chapter8/chapter8_exercise2.rb
+++ Chapter8/chapter8_exercise2.rb
@@ -1,23 +1,28 @@
 puts "計算を始めます"
 puts "何回計算を繰り返しますか"
 
-a = gets.to_i
+x = gets.to_i
 
 i = 1
 
-while i <= a do
-	puts i.to_s + "回目の計算"
+while i <= x do
+	puts i.to_s + "回目の計算" # "#{i}回目の計算"
 	puts "2つの値を入力してください"
 
 	a = gets.to_i
 	b = gets.to_i
 
-	puts "入力したaが#{a}"
-	puts "入力したbが#{b}"
+	puts "a=#{a}"
+	puts "b=#{b}"
+	puts "計算結果を出力します"
 	puts "a+b=#{a+b}"
 	puts "a-b=#{a-b}"
 	puts "a*b=#{a*b}"
 	puts "a/b=#{a/b}"
 
-	i.to_i += 1
+	i += 1
+
 end
+
+puts "計算を終了します"
+
